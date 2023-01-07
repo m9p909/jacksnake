98b85b78bf533692afd0a09e9d41766347aaf64d
Jack Clarke <jackfulcher09@gmail.com>
2023-01-07 10:13:30 -0500

    Finished the main minimax function
    still need to fill in the stubs

--- minmax/minmax.go
+++ minmax/minmax.go
@@ -37,42 +37,101 @@ func min(a float64, b float64) float64 {
 	}
 }
 
+type possibleGameState struct {
+	state GameState
+	move  string
+}
+
+func getPossibleStates(state GameState, turn int) []possibleGameState {
+	//TODO
+	return []possibleGameState{}
+
+}
+
 func (minmax *MinMaxImpl) evaluateState(state GameState) float64 {
+	// TODO
 	return 0
 }
 
-func getPossibleStates(state GameState) []*GameState {
-
+type MinimaxDataInput struct {
+	depth    int
+	playerId int
+	alpha    float64
+	beta     float64
+	state    GameState
 }
 
-func (minmax *MinMaxImpl) minimax(depth int, isMaximizingPlayer bool, alpha float64, beta float64, state GameState) float64 {
+type MinimaxDataOutput struct {
+	value float64
+	turn  string
+}
 
-	if depth == minmax.maxDepth {
-		return minmax.evaluateState(state)
+func (minmax *MinMaxImpl) minimax(input MinimaxDataInput) MinimaxDataOutput {
+
+	if input.depth == minmax.maxDepth {
+		state := minmax.evaluateState(input.state)
+		return MinimaxDataOutput{
+			value: state,
+			turn:  "unknown",
+		}
 	}
 
-	if isMaximizingPlayer {
+	if input.playerId == 0 {
 		bestVal := math.Inf(-1)
-		for _, child := range node.children {
-			value := minimax(child, depth+1, false, alpha, beta)
-			bestVal = max(bestVal, value)
-			alpha = max(alpha, bestVal)
-			if beta <= alpha {
+		bestState := possibleGameState{}
+		states := getPossibleStates(input.state, input.playerId)
+		for _, possibleState := range states {
+
+			minimaxOutput := minmax.minimax(MinimaxDataInput{
+				input.depth + 1,
+				input.playerId + 1,
+				input.alpha,
+				input.beta,
+				possibleState.state})
+
+			if minimaxOutput.value > bestVal {
+				bestVal = minimaxOutput.value
+				bestState = possibleState
+			}
+
+			input.alpha = max(input.alpha, bestVal)
+			if input.beta <= input.alpha {
 				break
 			}
 		}
-		return bestVal
+		return MinimaxDataOutput{
+			value: bestVal,
+			turn:  bestState.move,
+		}
+
 	} else {
-		bestVal := math.Inf(1)
-		for _, child := range node.children {
-			value := minimax(child, depth+1, true, alpha, beta)
-			bestVal = min(bestVal, value)
-			beta = min(beta, bestVal)
-			if beta <= alpha {
+		worstVal := math.Inf(1)
+		worstState := possibleGameState{}
+		states := getPossibleStates(input.state, input.playerId)
+		for _, possibleState := range states {
+
+			minimaxOutput := minmax.minimax(MinimaxDataInput{
+				input.depth + 1,
+				input.playerId + 1,
+				input.alpha,
+				input.beta,
+				possibleState.state})
+
+			if minimaxOutput.value < worstVal {
+				worstVal = minimaxOutput.value
+				worstState = possibleState
+			}
+
+			input.beta = min(input.beta, worstVal)
+			if input.beta <= input.alpha {
 				break
 			}
 		}
-		return bestVal
+
+		return MinimaxDataOutput{
+			value: worstVal,
+			turn:  worstState.move,
+		}
 	}
 
 }
