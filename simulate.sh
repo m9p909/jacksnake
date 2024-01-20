c66c90c46805d312cba58ef94e6e3a17d7c32284
Jack Clarke <jackfulcher09@gmail.com>
2024-01-20 09:31:06 -0500

    add simulate command

---
+++ simulate.sh
@@ -0,0 +1,6 @@
+battlesnake play -W 11 -H 11\
+  --name Snake1 --url http://localhost:8000\
+  --name Snake2 --url http://localhost:8000 \
+  --name Snake3 --url http://localhost:8000 \
+  --name Snake4 --url http://localhost:8000 \
+  --browser
