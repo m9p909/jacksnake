96c6cdaf7fe4794fc99edc322176599df980ed24
Jack Clarke <jackfulcher09@gmail.com>
2024-01-20 13:17:41 -0500

    Update depth count

--- minimaxplayer/buildMinimaxPlayer.go
+++ minimaxplayer/buildMinimaxPlayer.go
@@ -24,7 +24,7 @@ func BuildRandomPlayer() Player {
 
 func BuildMinimaxPlayer() Player {
 	conv := StateConverterImpl{}
-	algo := coreplayer.NewMinimaxAlgoMove(customsimulator.New(), evaluator.NewSimpleEvaluator(), 8)
+	algo := coreplayer.NewMinimaxAlgoMove(customsimulator.New(), evaluator.NewSimpleEvaluator(), 7)
 	player := MinimaxPlayer{}
 	player.Init(&conv, algo)
 	return &player
