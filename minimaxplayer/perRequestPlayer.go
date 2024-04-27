ebca119b5d3a6ca2313841bebddfce5bff61fa90
Jack Clarke <jackfulcher09@gmail.com>
2024-04-27 09:44:10 -0400

    scale depth based on number of snakes

--- minimaxplayer/perRequestPlayer.go
+++ minimaxplayer/perRequestPlayer.go
@@ -19,7 +19,13 @@ func (player *PerRequestPlayer) Move(state models.GameState) string {
 	// printJson(state)
 	// clone the player
 	core, id := player.converter.StateToCore(state)
-	algo := coreplayer.NewMinimaxAlgoMove(customsimulator.New(), evaluator.NewVoronoiEval(), 6)
+	total := 0
+	for _, snake := range core.Snakes {
+		if snake.Health > 0 {
+			total++
+		}
+	}
+	algo := coreplayer.NewMinimaxAlgoMove(customsimulator.New(), evaluator.NewVoronoiEval(), 10-total)
 	move := algo.Move(core, id)
 	return coreplayer.DirectionToString(move)
 }
